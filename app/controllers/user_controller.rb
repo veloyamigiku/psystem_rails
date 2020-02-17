2f1c9c3681f888eb3b0ede86c763300149665435
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2020-02-17 08:52:12 +0000

    ・CSRF対策を一時的に無効にする。 ・利用者登録のルーティングのメソッドをPOSTに修正する。 ・利用者登録アクションで、リクエスト情報を取得する。

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,15 @@
 class UserController < ApplicationController
 
     def register
-        sample = RegisterResult.new(true)
+        # リクエストパラメータから、利用者登録情報を取得する。
+        user = params[:user]
+        password = params[:password]
+        username = params[:username]
+
+        # 利用者登録結果を送信する。
+        registerResult = RegisterResult.new(true)
         render json: sample.toHash
+        
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  get 'user/register' => 'user#register'
+  
+  post 'user/register' => 'user#register'
   
 end
